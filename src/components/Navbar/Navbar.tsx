6d691aaa8d55347f6322e1eb3eb0f28ded93ce52
iskewedi <jtornello2019@hotmail.com>
2022-01-18 16:24:52 -0300

    -Added ref and styles params in the Button component. -Improved Navbar styles. -Added new Scrollable component and its styles, and implemented it in Navbar.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,22 +1,47 @@
+import React, { useState, useRef, createRef } from 'react';
 import Button from '../common/Button';
-import DeleteIcon from '../common/icons/DeleteIcon';
-import DownloadIcon from '../common/icons/DownloadIcon';
+import SettingsIcon from '../common/icons/SettingsIcon';
 import TimerIcon from '../common/icons/TimerIcon';
-import Toggle from '../common/Toggle';
+import Scrollable from '../Scrollable/Scrollable';
 import styles from './navbar.module.css';
+import { uuid } from '../../service/utils';
 
 export interface INavbarProps {
   profiles: string[];
 }
 
 const Navbar = ({ profiles }: INavbarProps) => {
+  const [active, setActive] = useState<number>(0);
+
+  const handleChange = (index: number) => {
+    setActive(index);
+  };
+
+  const containerRef = createRef<HTMLDivElement>();
+
+  const buttonsRefs = useRef<React.RefObject<HTMLButtonElement>[]>(
+    Array.from(profiles, () => React.createRef<HTMLButtonElement>())
+  );
+
   return (
-    <div className={styles.container}>
-      <Toggle options={profiles} containerClass={styles.profilesContainer} />
+    <div className={styles.container} ref={containerRef}>
+      <Scrollable containerClasses={styles.profilesContainer} containerRef={containerRef}>
+        {profiles.map((title, index) => (
+          <Button
+            key={uuid()}
+            classes={`${styles.profile} ${
+              (index === active && styles.profileActive) || ''
+            }`}
+            title={title}
+            onClick={() => handleChange(index)}
+            ref={buttonsRefs.current[index]}
+          />
+        ))}
+      </Scrollable>
+
       <div className={styles.buttonsContainer}>
-        <Button children={<TimerIcon width={18} />} />
-        <Button children={<DownloadIcon width={18} />} />
-        <Button children={<DeleteIcon width={18} />} />
+        <Button children={<TimerIcon width={24} />} />
+        <Button children={<SettingsIcon width={24} />} />
       </div>
     </div>
   );
---
+++ src/components/Scrollable/Scrollable.tsx
@@ -0,0 +1,227 @@
+import React, { CSSProperties, useCallback, useLayoutEffect, useState } from 'react';
+import { uuid } from '../../service/utils';
+import Button from '../common/Button';
+import ArrowIcon, { Direction } from '../common/icons/ArrowIcon';
+import styles from './scrollable.module.css';
+
+interface DOMData {
+  containerWidth: number;
+  itemsWidth: number;
+  pagesCount: number;
+}
+
+interface IRefObject {
+  ref?: React.RefObject<Element>;
+}
+
+export enum IScrollableType {
+  Vertical = 1,
+  Horizontal,
+}
+
+interface IScrollableChildren {
+  style?: CSSProperties;
+}
+
+interface IScrollState {
+  isScrolling: boolean;
+  itemsOffset: {
+    x: number;
+    y: number;
+  };
+  currentPage: number;
+  pagesCount: number;
+  itemsWidth: number;
+}
+
+const CHILDREN_CONTAINER_ID = uuid();
+
+export interface IScrollableProps {
+  containerRef: React.RefObject<HTMLDivElement>;
+  containerClasses?: string;
+  slideTimeMs?: number;
+  children: React.ReactNode;
+}
+
+const Scrollable = ({
+  containerRef,
+  containerClasses = '',
+  slideTimeMs = 250,
+  children,
+}: IScrollableProps) => {
+  const [scrollState, setScrollState] = useState<IScrollState>({
+    isScrolling: false,
+    itemsOffset: {
+      x: 0,
+      y: 0,
+    },
+    currentPage: 0,
+    pagesCount: 0,
+    itemsWidth: 0,
+  });
+  const [mappedChildren, setMappedChildren] = useState<React.ReactNode[]>([]);
+
+  const { currentPage, pagesCount } = scrollState;
+
+  const canScrollLeft = currentPage > 0;
+  const canScrollRight = currentPage < pagesCount;
+
+  const handleBtnScroll = (direction: Direction) => {
+    const { isScrolling, itemsOffset, currentPage, itemsWidth } = scrollState;
+
+    if (isScrolling) return;
+
+    let newOffset: IScrollState['itemsOffset'] = { ...itemsOffset };
+    let newPage = currentPage;
+
+    if (direction === Direction.Left) {
+      if (!canScrollLeft) return;
+      const distance = 3 * itemsWidth;
+
+      newOffset.x += distance;
+      newPage -= 1;
+    }
+
+    if (direction === Direction.Right) {
+      if (!canScrollRight) return;
+      const distance = -3 * itemsWidth;
+
+      newOffset.x += distance;
+      newPage += 1;
+    }
+
+    setScrollState(state => ({ ...state, isScrolling: true, itemsOffset: newOffset }));
+
+    setTimeout(
+      () =>
+        setScrollState(state => ({ ...state, isScrolling: false, currentPage: newPage })),
+      slideTimeMs - 100
+    );
+  };
+
+  const getDOMData = useCallback(
+    (childrenRefs: React.RefObject<Element>[], container: HTMLDivElement): DOMData => {
+      const data: DOMData = {
+        containerWidth: 0,
+        pagesCount: 0,
+        itemsWidth: 0,
+      };
+
+      const childrenContainer = Array.from(container.children).find(
+        child => child.id === CHILDREN_CONTAINER_ID
+      );
+
+      if (childrenContainer) {
+        const containerInfo = childrenContainer.getBoundingClientRect();
+
+        data.containerWidth = containerInfo.width;
+      }
+
+      const itemsWidth: number[] = [];
+
+      let currentPageWidth = 0;
+
+      const totalPages = childrenRefs.reduce((previous, currentRef): number => {
+        if (!currentRef.current) return previous;
+
+        const info = currentRef.current.getBoundingClientRect();
+
+        itemsWidth.push(info.width);
+
+        currentPageWidth += info.width;
+
+        if (currentPageWidth >= data.containerWidth - 25) {
+          currentPageWidth = 0;
+          return previous + 1;
+        }
+
+        return previous;
+      }, 0);
+
+      data.pagesCount = totalPages;
+      data.itemsWidth = itemsWidth.reduce((a, b) => a + b, 0) / itemsWidth.length || 0;
+
+      return data;
+    },
+    []
+  );
+
+  const setDOMInfo = (childrenRefs: React.RefObject<Element>[]): void => {
+    let checkInterval: NodeJS.Timeout;
+
+    checkInterval = setInterval(() => {
+      if (childrenRefs[0] && childrenRefs[0].current && containerRef.current) {
+        const { pagesCount, itemsWidth } = getDOMData(childrenRefs, containerRef.current);
+
+        setScrollState(state => ({ ...state, pagesCount, itemsWidth }));
+
+        clearInterval(checkInterval);
+      }
+    }, 50);
+  };
+
+  useLayoutEffect(() => {
+    const childrenRefs: React.RefObject<Element>[] = [];
+
+    const mapped = React.Children.map(children, child => {
+      if (!React.isValidElement<IScrollableChildren>(child)) {
+        return child;
+      }
+
+      const elementChild: React.ReactElement & IRefObject = child;
+
+      const style: React.CSSProperties = {
+        transition: `transform ${slideTimeMs}ms ease-in-out`,
+      };
+      if (scrollState.itemsOffset.x) {
+        style.transform = `translateX(${scrollState.itemsOffset.x}px)`;
+      }
+
+      if (elementChild.ref) {
+        childrenRefs.push(elementChild.ref);
+      }
+
+      return React.cloneElement(child, {
+        style,
+        ...child.props,
+      });
+    });
+
+    setDOMInfo(childrenRefs);
+
+    mapped && setMappedChildren(mapped);
+  }, [children, scrollState.isScrolling, scrollState.itemsOffset]);
+
+  return (
+    <React.Fragment>
+      {canScrollLeft && (
+        <div>
+          <Button
+            onClick={() => handleBtnScroll(Direction.Left)}
+            classes={styles.arrowsButtons}
+          >
+            <ArrowIcon width={15} height={15} />
+          </Button>
+        </div>
+      )}
+      <div
+        id={CHILDREN_CONTAINER_ID}
+        className={`${containerClasses} ${styles.container}`}
+      >
+        {mappedChildren}
+      </div>
+      {canScrollRight && (
+        <div className={styles.rightArrow}>
+          <Button
+            onClick={() => handleBtnScroll(Direction.Right)}
+            classes={styles.arrowsButtons}
+          >
+            <ArrowIcon direction={Direction.Right} width={15} height={15} />
+          </Button>
+        </div>
+      )}
+    </React.Fragment>
+  );
+};
+
+export default Scrollable;
